3b3e939371c17558b4a3cedf5bc4c0c39bb535b1
Peaches <caroline.memishian@vanderbilt.edu>
2017-04-23 23:50:54 -0500

    peche

--- src/com/memishian/tictactoe/TicTacToe2.java
+++ src/com/memishian/tictactoe/TicTacToe2.java
@@ -19,28 +19,75 @@ public class TicTacToe2 {
 		System.out.println("Tic Tac Toe Server is running");
 		try{
 			while(true){
-				Game game = new Game(); 
-				
+				TTTGame game = new TTTGame(); 
+				TTTGame.Player playerX = game.new Player(listen.accept(), 'X');
+				TTTGame.Player playerO = game.new Player(listen.accept(), 'O');
+				playerX.setOpponent(playerO);
+				playerO.setOpponent(playerX);
+				game.currentPlayer = playerX;
+				playerX.start();
+				playerO.start();
 			}
 		}finally{
 			listen.close(); 
 		}
 	}
-	
+}
 	//implements a 2 player game
-	class TTTGame{
+class TTTGame{
 		//a board has 9 squares and each is either clear or 
 		//has a marker in it put there by a player 
 		//use an array of player references - if null the 
 		//corresponding square is unowned, otherwise the array
 		//cell stores a reference to the player that owns it
-		private Player[] board ={
-				null, null, null, 
-				null, null, null, 
-				null, null, null}; 
-			
+	private Player[] board ={
+			null, null, null, 
+			null, null, null, 
+			null, null, null}; 
 		
+	//who is currently playing
+	Player currentPlayer; 
+	
+	//will return current state of board
+	//if the current board has a winner
+	//check all the options that could make a win 
+	//check horizontal wins first and then vertical
+	public boolean hasWinner(){
+		return
+				(board[0] != null && board[0]==board[1] && board[0] == board[2])
+				|| (board[3] != null && board[3] == board[4] && board[3] == board[5])
+				|| (board[6] != null && board[6] == board[7] && board[6] == board[8])
+				||(board[0] != null && board[0] == board[3] && board[0] == board[6])
+				||(board[1] != null && board[1] == board[4] && board[1] == board[7])
+				||(board[2] != null && board[2] == board[5] && board[2] == board[8])
+				||(board[0] != null && board[0] == board[4] && board[0] == board[8])
+				||(board[2] != null && board[2] == board[4] && board[2] == board[6]);
+	}
+	
+	//this will check to see if there are no more empty spaces in the board
+	public boolean boardFilledUp(){
+		for(int i=0; i<board.length; i++){
+			if(board[i] == null){
+				return false; 
+			}
 		}
+		return true;
+	}
+	
+	//this will check if the next move is legal
+	//aka that the current player is the one requesting th emove
+	//and the square is not already filled
+	//if it is legal then the game will be updated
+	//next player is notified 
+	public synchronized boolean legalMove(int location, Player player){
+		if(player == currentPlayer && board[location] == null){
+			board[location] = currentPlayer;
+			currentPlayer = currentPlayer.opponent;
+			currentPlayer.otherPlayerMoved(location);
+			return true;
+		}
+		return false;
+	}
 	
 	//this is a class for the helper threads in the multi 
 	//threaded server application. A player is identified by a 
@@ -116,15 +163,8 @@ public class TicTacToe2 {
 				try{
 					socket.close();
 				}catch(IOException e){}
+			}	
 			}
-			
-		}
-
-		
-		
+		}	
 	}
-		
-		
-	}
-	
 }
